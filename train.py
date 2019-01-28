cd4c326866ef7c65bae56218e9553e2b88915404
platonovsimeon <41298320+platonovsimeon@users.noreply.github.com>
2019-01-28 22:06:01 +0200

    Update train.py

--- train.py
+++ train.py
@@ -39,12 +39,12 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
-        'data/train',target_size=(64,64),
+        'dataset/train',target_size=(64,64),
         batch_size=20,
         class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(
-        'data/validation',
+        'dataset/validation',
         target_size=(64,64),
         batch_size=20,
         class_mode='categorical')
